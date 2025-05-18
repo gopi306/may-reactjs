8e7f002d9cc96337d4ef75a3097cf8962ca2a61b
Gopi Nath <gopinathreddy4362@gmail.com>
2025-05-18 09:40:47 +0530

    Version3

--- src/BharathClock/demo.jsx
+++ src/BharathClock/demo.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const demo = () => {
-  return <div>i am a hero</div>;
+  return (
+    <div>
+      i am a hero
+      <h1>I did not understand git</h1>
+    </div>
+  );
 };
 
 export default demo;
